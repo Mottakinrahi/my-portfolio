3cf20037e583b18288f40ca0e1209b75d5d912a3
Mottakinrahi <mottakin.chy@gmail.com>
2025-11-16 10:49:39 +0600

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/my-portfolio/',
-});
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
+  base: '/my-portfolio/', 
+})
+
